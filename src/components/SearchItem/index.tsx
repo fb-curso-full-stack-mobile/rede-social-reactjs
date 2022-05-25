29364d7ba5e19364a6133bbae86773e261d0dd95
Fabio Bergmann <fabiocberg@gmail.com>
2022-05-25 03:22:09 -0300

    feat: solicitação de amizade

--- src/components/SearchItem/index.tsx
+++ src/components/SearchItem/index.tsx
@@ -1,15 +1,41 @@
 import { User, fullname } from "../../models/user";
+import { useEffect, useState } from "react";
 
 import Box from "../Box";
 import Button from "../Button";
+import { Friend } from "../../models/friend";
 import UserPhoto from "../UserPhoto";
 import styles from "./styles.module.css";
+import { useFetch } from "../../hooks/useFetch";
 
 type SearchItemProps = {
     user: User;
 };
 
 export default function SearchItem({ user }: SearchItemProps) {
+    const [friend, setFriend] = useState<Friend | undefined | null>(undefined);
+    const { request, response, clear } = useFetch<any>();
+
+    useEffect(() => {
+        console.log("request");
+        // verificar se já existe uma solitação
+        if (friend === undefined) {
+            request(`/api/v1/friend/${user.id}`);
+        }
+    }, [friend, request, user.id]);
+
+    useEffect(() => {
+        console.log("response: ", response);
+        if (response) {
+            setFriend(response.friend);
+            clear();
+        }
+    }, [clear, response]);
+
+    const sendFriendRequest = () => {
+        request("/api/v1/friend", "POST", { friendId: user.id });
+    };
+
     return (
         <Box className={styles.container}>
             <div className={styles.userData}>
@@ -18,7 +44,15 @@ export default function SearchItem({ user }: SearchItemProps) {
                     <div>{fullname(user) || "-"}</div>
                 </div>
             </div>
-            <Button onClick={() => {}}>Solicitar Amizade</Button>
+            {friend ? (
+                friend.accepted ? (
+                    <div>Amigos!</div>
+                ) : (
+                    <div>Solicitação enviada!</div>
+                )
+            ) : friend === null ? (
+                <Button onClick={sendFriendRequest}>Solicitar Amizade</Button>
+            ) : null}
         </Box>
     );
 }
---
+++ src/models/friend.ts
@@ -0,0 +1,7 @@
+export type Friend = {
+    userAId: number;
+    userBId: number;
+    accepted: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+};
